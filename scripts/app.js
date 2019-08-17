ae0a9003d294836f39dbf7b433011ce057a28f56
wesauis <54046750+wesauis@users.noreply.github.com>
2019-08-16 22:09:40 -0300

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,38 @@
-(function() {
-  async function fetchWeather() {
-    const position = await getGeo();
+(async function() {
+  const position = await getGeo();
 
-    const { latitude: lat, longitude: lon } = position.coords;
-    const apiKey = "72f8ea053a2b1d9bb5209c9d4935727";
-    const apiUrl = `https://api.darksky.net/forecast/${apiKey}/${lat},${lon}`;
+  const { latitude: lat, longitude: lon } = position.coords;
 
-    console.log(await fetch(apiUrl));
+  const apiKey = "72f8ea053a2b1d9bb5209c9d4935727";
+  const apiUrl = `https://api.darksky.net/forecast/${apiKey}/${lat},${lon}/?exclude=minutely,hourly,daily,alerts,flags`;
 
-    var skycons = new Skycons({ color: "white" });
-    skycons.add("icon", Skycons.PARTLY_CLOUDY_DAY);
-    skycons.play();
-  }
+  const response = await fetch(apiUrl);
+  if (!response) return;
+
+  const {
+    currently: { icon, summary, temperature }
+  } = await response.json();
+
+  // set geolocation
+  const coords = document.querySelector("#geo h3");
+  coords.innerHTML = `lat: ${lat}  lon: ${lon}`;
+
+  //
+  const description = document.querySelector("#desc");
+  description.innerHTML = summary;
+
+  // degree
+  const degree = document.querySelector("#temp #degree");
+  degree.innerHTML = degree;
+
+  // ºF
+  const temp = document.querySelector("#temp unit");
+  temp.innerHTML = temperature;
+
+  // set icon
+  var skycons = new Skycons({ color: "white" });
+  skycons.add("icon", Skycons[icon.replace("-", "_").toUpperCase()]);
+  skycons.play();
 
   async function getGeo() {
     if (!("geolocation" in navigator)) {
