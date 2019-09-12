bedca688787e28ad0edbca60f05ea074e1f217b2
W2GOfficial <wesleya.isotton@gmail.com>
2019-09-12 15:43:08 -0300

    added an "reveal" animation on data set

--- scripts/app.js
+++ scripts/app.js
@@ -9,17 +9,15 @@ async function start() {
     ).then(forecast => forecast.currently);
     console.log('Weather forecast obtained');
 
-    // set geolocation
-    $('.geo-sec #lat').innerHTML = `lat: ${latitude.toFixed(2)}`;
-    $('.geo-sec #lon').innerHTML = `lon: ${longitude.toFixed(2)}`;
-    // set description
-    $('#description').innerHTML = summary;
-    // set degree and add option to swap beetwen ºC and ºF onClick
-    const degreeEl = $('.temp-sec #degree');
-    const swapper = new DegreeSwapper(degreeEl, temperature);
-    degreeEl.addEventListener('click', () => swapper.swap(false));
+    setThings({
+      latitude,
+      longitude,
+      icon,
+      summary,
+      temperature,
+    });
 
-    setIcon(icon).then(() => console.log('Weather Icon Set'));
+    $('#app').classList.toggle('hidden');
   } catch (error) {
     switch (error) {
       case ERROR.NO_GEOLOCATION_SUPPORT:
@@ -36,4 +34,18 @@ async function start() {
         console.error(error);
     }
   }
+}
+
+async function setThings({ latitude, longitude, summary, temperature, icon }) {
+  // set geolocation
+  $('.geo-sec #lat').innerHTML = `lat: ${latitude.toFixed(2)}`;
+  $('.geo-sec #lon').innerHTML = `lon: ${longitude.toFixed(2)}`;
+  // set description
+  $('#description').innerHTML = summary;
+  // set degree and add option to swap beetwen ºC and ºF onClick
+  const degreeEl = $('.temp-sec #degree');
+  const swapper = new DegreeSwapper(degreeEl, temperature);
+  degreeEl.addEventListener('click', () => swapper.swap(false));
+  // setIcon
+  setIcon(icon).then(() => console.log('Weather Icon Set'));
 }
